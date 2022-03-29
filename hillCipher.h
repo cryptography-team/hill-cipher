a93908b80ae259fd4241db8673d5d443c865d4d9
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-03-30 00:37:03 +0200

    included random, chrono.. added rng variable for random ops.. modified constructor and encrypt declarations

--- hillCipher.h
+++ hillCipher.h
@@ -1,8 +1,9 @@
 #ifndef HILL_CIPHER_HEADER
-
 #define HILL_CIPHER_HEADER
 
 #include "matrix.h"
+#include <chrono>
+#include <random>
 #include <string>
 
 using std::string;
@@ -10,16 +11,21 @@ using std::string;
 class hillCipher {
 private:
   enum { ALPHABETS = 26 };
-  matrix<int> *key, *reverseKey;
+  matrix<int> key, *reverseKey;
   int coprimeTo26[12], inverse[ALPHABETS];
+  std::mt19937 rng;
 
   void fixReverseKeyIfDamaged(); // Mansour
   void damageReverseKey();       // Mansour
 
 public:
-  hillCipher(int size = 3); // Mansour: initialize key, reverseKey, coprimeTo26,
-                            // inverse + generateRandomKey
-  ~hillCipher();            // Mansour
+  hillCipher(
+      int size =
+          3); // Mansour: initialize key, reverseKey, coprimeTo26,
+              // inverse + generateRandomKey
+              // initialize rng as stated below:
+              // rng(std::chrono::steady_clock::now().time_since_epoch().count())
+  ~hillCipher(); // Mansour
 
   void generateRandomKey(); // Mansour
   bool isValidKey(const matrix<int> &key) const;
@@ -28,7 +34,7 @@ public:
   matrix<int> getKey() const;
   matrix<int> getReverseKey();
 
-  string encrypt(const string &plainText) const;
+  string encrypt(const string &plainText, const char &dummyLetter = 'a') const;
   string decrypt(const string &cipherText); // Mansour
 };
 
