60c3385f328a847950cea5864ecd07912e729980
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-03-30 22:55:52 +0200

    testing generateRandomKey

--- main.cpp
+++ main.cpp
@@ -1,12 +1,5 @@
+#include "hillCipher.h"
 #include "matrix.h"
 #include <iostream>
 using namespace std;
-int main() {
-  int n;
-  cin >> n;
-  matrix<int> x(n, n);
-  for (int i = 0; i < n; i++)
-    for (int j = 0; j < n; j++)
-      cin >> x(i, j);
-  cout << x;
-}
+int main() { hillCipher x; }
