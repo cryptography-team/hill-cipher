5bb5beab4684b23f0612c1354c02193af20e9c69
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-03-30 22:55:30 +0200

    fixed some issues with constructor, rowAddition, generateRandomKey

--- hillCipher.cpp
+++ hillCipher.cpp
@@ -5,39 +5,42 @@ void hillCipher::fixReverseKeyIfDamaged() {}
 
 void hillCipher::damageReverseKey() {}
 
-hillCipher::hillCipher(const int &size) : rng(std::chrono::steady_clock::now().time_since_epoch().count()){
-    key = matrix<int>(size,size); generateRandomKey(); //initialize key matrix and generate it randomlly
-    reverseKey = new matrix<int>(size,size);   //initialize reverse key matrix
-    for(int i = 0,j=1 ; i<12  ; i++ , j+=2){
-        if(j == 13) j+= 2;
-        coprimeTo26[i] = j;}
-
+hillCipher::hillCipher(const int &size)
+    : rng(std::chrono::steady_clock::now().time_since_epoch().count()),
+      key(size, size) {
+  for (int i = 0, j = 1; i < 12; i++, j += 2) {
+    if (j == 13)
+      j += 2;
+    coprimeTo26[i] = j;
+  }
+  generateRandomKey(); // initialize key matrix and generate it randomlly
 }
 
 hillCipher::~hillCipher() {}
 
 void hillCipher::generateRandomKey() {
-    using std::uniform_int_distribution;
+  using std::uniform_int_distribution;
 
-    int len = key.getRows();
-    for(int i = 0 ; i<len ; i++){
-            key(i,i) = coprimeTo26[uniform_int_distribution<int>(0,12)(rng)];}
-            cout<<key<<endl <<key.determinant();
-    for(int i = 0 ;i<ALPHABETS;i++){
-            rowAddition(uniform_int_distribution<int>(0,len-1)(rng)
-                            ,uniform_int_distribution<int>(0,len-1)(rng)
-                            ,uniform_int_distribution<int>(0,ALPHABETS-1)(rng));}
+  int len = key.getRows();
+  for (int i = 0; i < len; i++) {
+    key(i, i) = coprimeTo26[uniform_int_distribution<int>(0, 11)(rng)];
+  }
+  cout << key << key.determinant() % ALPHABETS << endl;
+  for (int i = 0; i < ALPHABETS; i++) {
+    rowAddition(uniform_int_distribution<int>(0, len - 1)(rng),
+                uniform_int_distribution<int>(0, len - 1)(rng),
+                uniform_int_distribution<int>(0, ALPHABETS - 1)(rng));
+  }
+  cout << key << key.determinant() % ALPHABETS + ALPHABETS << endl;
 }
 
-void hillCipher :: rowAddition(int mulRow, int additionRow,int mulVal){
-    int len = key.getRows();
-    if(mulRow > len || additionRow > len || mulRow == additionRow) return;
-        for(int i = 0 ; i<len ; i++)
-           {
-               key(additionRow,i) += ((key(mulRow,i)*mulVal)%ALPHABETS);
-               if(key(additionRow,i) >= ALPHABETS)
-                key(additionRow,i) %= ALPHABETS;
-           }
+void hillCipher::rowAddition(int mulRow, int additionRow, int mulVal) {
+  int len = key.getRows();
+  if (mulRow == additionRow)
+    return;
+  for (int i = 0; i < len; i++) {
+    key(additionRow, i) += ((key(mulRow, i) * mulVal) % ALPHABETS) % ALPHABETS;
+  }
 }
 
 bool hillCipher::isValidKey(const matrix<int> &key) const {
