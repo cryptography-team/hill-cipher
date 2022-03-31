3138eed9416cfbfa98c5a9dee82c23185f8cfe5e
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-03-31 20:20:07 +0200

    edited the interface to work on windows and linux

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
-#include "hillCipher.h"
-#include "matrix.h"
+//#include "hillCipher.h"
+//#include "matrix.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
-  hillCipher tool;
-  cout << "===================================================================="
-          "====\n";
+  // hillCipher tool;
+  cout
+      << "  ================================================================\n";
   cout << "\tHill Cipher tool was initiated with a random key.\n";
+  cout << "\tHope it will be a good interface. Use it as you wish :)\n";
   while (true) {
+    cout
+        << "  "
+           "================================================================\n";
     cout << "\tChoose one of the following options:\n";
     cout << "\t1- Encrypt a plain text using the current key.\n";
     // Other options here...
@@ -30,6 +34,14 @@ int main() {
     default:
       cout << "Please enter a valid number..." << endl;
     }
+#ifdef __unix__
+    system("clear");
+#else
+    system("cls");
+#endif
   }
+  cout
+      << "  ================================================================\n";
+  cout << "\tSad to see you go :(\n";
   return 0;
 }
