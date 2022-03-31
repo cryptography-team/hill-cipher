ed521434129fa5e42fb3ab5950b3e4968dfbb55c
mohamed Mansour <67611571+muns99@users.noreply.github.com>
2022-03-31 12:55:27 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ using namespace std;
 
 int main() {
   hillCipher tool;
+  string plain ="muns and yasser";
+  string cipher ;
+  matrix<int> key(3,3);
+  matrix<int>reversekey(3,3);
   cout
       << "  ================================================================\n";
   cout << "\tHill Cipher tool was initiated with a random key.\n";
@@ -29,10 +33,71 @@ int main() {
       break;
     switch (choice) {
     case 1:
+        char ch ;
+        cout<<"Existing plain text is : <" <<plain<<"> continue with this plain? press y if you or n if u don't : ";
+        cin>>ch;
+
+        if(ch == 'n')
+        {
+            cout<<"Set your plain : ";
+            plain = "";
+            cin>>plain;
+        }
+        cipher = tool.encrypt(plain);
+        cout<<"\n"<<cipher;
 
       break;
     case 2:
+        cout<<tool.decrypt(cipher);
 
+      break;
+    case 3:
+        cout<<tool.getKey();
+        break;
+    case 4:
+        cout<<tool.getReverseKey();
+        break;
+    case 5:
+        int siz;
+        cout<<"Set your key size : ";
+        cin >> siz;
+        key = matrix<int>(siz,siz);
+        reversekey = matrix<int>(siz,siz);
+        cout<<"Enter your key row by row with space between columns... \n";
+        for(int i = 0 ;i<siz;i++){
+            for(int j = 0;j<siz;j++){
+                cin>>key(i,j);
+                cout<<" ";
+            }
+            cout<<"\n";
+        }
+        if(!tool.isValidKey(key))
+            cout<<"Not valid key \n";
+        break;
+    case 6:
+        tool.generateRandomKey();
+        break;
+    case 7:
+        exit(-1);
+        break;
+    // Other cases here...
+    default:
+      cout << "\tPlease enter a valid number..." << endl;
+    }
+    cout << "\n\tPress any key to continue..." << endl;
+#ifdef __unix__
+    system("read");
+    system("clear");
+#else
+    system("pause");
+    system("cls");
+#endif
+  }
+  cout
+      << "  ================================================================\n";
+  cout << "\tSad to see you go :(\n";
+  return 0;
+}
       break;
     // Other cases here...
     default:
