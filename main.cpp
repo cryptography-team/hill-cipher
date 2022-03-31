34d72e965af3a00ce6cedd6381ccd8538dcad00e
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-03-31 20:02:59 +0200

    added the structure of the interface

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include "hillCipher.h"
 #include "matrix.h"
 #include <iostream>
+
 using namespace std;
+
 int main() {
-  hillCipher x;
-  cout << x.getKey();
+  hillCipher tool;
+  cout << "===================================================================="
+          "====\n";
+  cout << "\tHill Cipher tool was initiated with a random key.\n";
+  while (true) {
+    cout << "\tChoose one of the following options:\n";
+    cout << "\t1- Encrypt a plain text using the current key.\n";
+    // Other options here...
+    cout << "\t7- Exit the program.\n";
+    cout << "\n\tYour choice is [1, 7]: " << flush;
+    int choice;
+    cin >> choice;
+    if (choice == 7)
+      break;
+    switch (choice) {
+    case 1:
+
+      break;
+    case 2:
+
+      break;
+    // Other cases here...
+    default:
+      cout << "Please enter a valid number..." << endl;
+    }
+  }
+  return 0;
 }
