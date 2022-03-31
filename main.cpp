1d63e731dd1fba49eb04ad022ee3ed5e519670d1
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-04-01 01:04:24 +0200

    added files to i/o for any entered text

--- main.cpp
+++ main.cpp
@@ -1,8 +1,48 @@
 #include "hillCipher.h"
 #include "matrix.h"
+#include <fstream>
 #include <iostream>
 
 using namespace std;
+
+void getText(const string &type, string &text) {
+  cout << "\tThe last " << type << " text was \"" << text
+       << "\".. Continue with this " << type << " text? (Y/n): " << flush;
+  char ch;
+  cin >> ch;
+  if (ch == 'n') {
+    cout << "\tSet your " << type << " text from a file? (y/N): " << flush;
+    cin >> ch;
+    if (ch == 'y') {
+      cout << "\tEnter file name: " << flush;
+      string tmp;
+      cin >> tmp;
+      ifstream in(tmp.c_str());
+      text.clear();
+      while (getline(in, tmp))
+        text += tmp;
+    } else {
+      cout << "\tSet your " << type << " text (in one line):" << endl;
+      cin.ignore();
+      getline(cin, text);
+    }
+  }
+}
+
+void printText(const string &type, const string &text) {
+  cout << "\tPrint to a file? (y/N): " << flush;
+  char ch;
+  cin >> ch;
+  if (ch == 'y') {
+    cout << "\tEnter file name: " << flush;
+    string tmp;
+    cin >> tmp;
+    ofstream out(tmp.c_str());
+    out << text << endl;
+  } else
+    cout << "\tThis is the corresponding " << type << " text:\n\t" << text
+         << endl;
+}
 
 int main() {
   hillCipher tool;
@@ -35,43 +75,28 @@ int main() {
       break;
     switch (choice) {
     case 1:
-      char ch;
-      cout << "\tThe last plain text was \"" << plain
-           << "\".. Continue with this plain text? (Y/n): " << flush;
-      cin >> ch;
-      if (ch == 'n') {
-        cout << "\tSet your plain text (in one line):" << endl;
-        cin.ignore();
-        getline(cin, plain);
-      }
+      getText("plain", plain);
       cout << "\tSet your dummy letter that will be used in case the plain "
            << "text was not long enough: " << flush;
       char dummyLetter;
       cin >> dummyLetter;
       cipher = tool.encrypt(plain, dummyLetter);
-      cout << "\tThis is the corresponding cipher text:\n\t" << cipher;
+      printText("cipher", cipher);
       break;
     case 2:
-      cout << "\tThe last cipher text was \"" << cipher
-           << "\".. Continue with the last cipher text? (Y/n): " << flush;
-      cin >> ch;
-      if (ch == 'n') {
-        cout << "\tSet your cipher text (in one line):" << endl;
-        cin.ignore();
-        getline(cin, cipher);
-      }
+      getText("cipher", cipher);
       plain = tool.decrypt(cipher);
       if (plain.empty() && !cipher.empty())
         cout << "\tNot a valid cipher.. Cipher's size is not divisible by "
              << "matrix size (add some dummy letters)\n";
-      if (cipher.empty() || !plain.empty())
-        cout << "\tThis is the corresponding plain text:\n\t" << plain;
+      else
+        printText("plain", plain);
       break;
     case 3:
-      cout << tool.getKey();
+      cout << tool.getKey() << endl;
       break;
     case 4:
-      cout << tool.getReverseKey();
+      cout << tool.getReverseKey() << endl;
       break;
     case 5:
       int size;
@@ -90,17 +115,17 @@ int main() {
         cout << "\tSad.. Not a valid key.. This key will not be set.";
       else
         cout << "\tPerfecto.. The key has been set.";
+      cout << endl;
       break;
     case 6:
       cout << "\tSet your key size: " << flush;
       cin >> size;
       tool.generateRandomKey(size);
-      cout << "\tThis random key has been set:\n" << tool.getKey();
+      cout << "\tThis random key has been set:\n" << tool.getKey() << endl;
       break;
     default:
-      cout << "\tPlease enter a valid option...";
+      cout << "\tPlease enter a valid option..." << endl;
     }
-    cout << endl;
 #ifdef __unix__
     system("read -p \"Press return key to continue...\"");
     system("clear");
